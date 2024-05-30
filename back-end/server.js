d3ab59e9a07fa84a96a5b3ebc2a411df794a5d30
kimyoungjo <yoyounn88@gmail.com>
2024-05-30 11:37:07 +0900

    build: mongodb, ejs 세팅 및 연결 완료

--- back-end/server.js
+++ back-end/server.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { MongoClient } = require("mongodb");
 
 app.use(cors()); // cors 설정
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+let db;
+const url =
+  "mongodb+srv://admin:as123123@kpkpkp.cau2nx4.mongodb.net/?retryWrites=true&w=majority&appName=kpkpkp";
+new MongoClient(url)
+  .connect()
+  .then((client) => {
+    console.log("DB연결성공");
+    db = client.db("forum");
+    app.listen(8080, () => {
+      console.log("http://localhost:8080 에서 서버 실행중");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/list", async (req, res) => {
+  try {
+    let result = await db.collection("post").find().toArray();
+    // 응답.render("list.ejs", { 글목록: result });
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    응답.status(500).send("데이터를 불러오는 중 오류가 발생했습니다.");
+  }
+});
 
 app.get("/", (req, res) => {
   const data = [
@@ -11,11 +41,11 @@ app.get("/", (req, res) => {
     { name: "부모님 집", description: "신월5동 13-9" },
     { name: "헬스장", description: "서울 강남구 테헤란로8길 7" },
   ];
-
+  console.log("Server restarted at", new Date());
   res.send(data);
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
